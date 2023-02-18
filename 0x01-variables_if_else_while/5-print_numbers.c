f949a946d553b0d62ca1d3da6711ee58d27ed3a1
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-02-18 00:18:36 +0000

    C - If, else and while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Output numbers
+ *
+ *Return: 0 after success
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - output digits
+ *
+ *Return: 0 after success
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar(digit + '0');
+	putchar(10);
+	return (0);
+}
