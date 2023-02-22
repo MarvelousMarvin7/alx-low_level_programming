fe5d4e718aae98afe2fda9b3d610a2027bc83faf
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-02-22 01:38:28 +0000

    C - functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - output sum of 2 numbers
+ * @n: 1st variable
+ * @m: 2nd variable
+ *
+ * Return: sum of numbers
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
