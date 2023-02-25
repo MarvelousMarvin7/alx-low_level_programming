fd514d49fabf31929dd89dec4e46f9d49e337fc8
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-02-25 01:49:34 +0000

    C - More funtions and nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Checks code
+ *
+ * Return: 0 After Success
+ */
+void more_numbers(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			k = j / 10;
+			l = j % 10;
+			if (j > 9)
+			{
+				_putchar(k + '0');
+			}
+			_putchar(l + '0');
+		}
+		_putchar(10);
+	}
+}
