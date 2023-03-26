b6065a84b21ca732d29d6e66b6980ae51c280ce6
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-03-26 19:10:32 +0000

    C - Malloc and free

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,66 @@
 #include <stdio.h>
 
 /**
+ * print_char - print character
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	printf("%c", c);
+}
+
+/**
+ * print_int - print numbers
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+
+void print_int(va_list args)
+{
+	int i;
+
+	i = va_arg(args, int);
+	printf("%d", i);
+}
+
+/**
+ * print_float - print decimal numbers
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_float(va_list args)
+{
+	float f;
+
+	f = va_arg(args, double);
+	printf("%f", f);
+}
+
+/**
+ * print_string - print string
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_string(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char*);
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
  * print_all - prints anything
  * @format: list of types of arguments
  *
@@ -10,42 +70,27 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	char c;
-	int i = 0;
-	float f;
-	char *s;
+	int i = 0, j = 0;
+	void (*print_func[])(va_list) = {print_char, print_int, print_float,
+					 print_string};
 
 	va_start(args, format);
-	while (format != NULL && format[i])
+
+	while (format && format[i])
 	{
-		switch (format[i])
-		{
-		case 'c':
-			c = va_arg(args, int);
-			printf("%c", c);
-			break;
-		case 'i':
-			i = va_arg(args, int);
-			printf("%d", i);
-			break;
-		case 'f':
-			f = va_arg(args, double);
-			printf("%f", f);
-			break;
-		case 's':
-			s = va_arg(args, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s", s);
-			break;
-		default:
-			break;
-		}
-		if (format[i + 1] && (format[i] == 'c' || format[i] == 'i' ||
-				      format[i] == 'f' || format[i] == 's'))
+		j = 0;
+		while (j < 4 && format[i] != "cifs"[j])
+			j++;
+
+		if (j < 4)
+			print_func[j](args);
+
+		if (format[i + 1] && j < 4)
 			printf(", ");
+
 		i++;
 	}
+
 	printf("\n");
 
 	va_end(args);
