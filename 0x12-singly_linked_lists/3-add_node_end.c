701b4a527a7124e040f02ba5eada23e7f304f320
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-04-09 01:03:36 +0000

    Singly linked list (Add Node at the end)

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *ptr = NULL, *temp;
 
 	temp = malloc(sizeof(list_t));
-	if (ptr == NULL)
+	if (temp == NULL)
 		return (NULL);
 
 	temp->str = strdup(str);
@@ -23,6 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	temp->len = strlen(str);
 	temp->next = NULL;
+
 	if (*head == NULL)
 		*head = temp;
 	else
