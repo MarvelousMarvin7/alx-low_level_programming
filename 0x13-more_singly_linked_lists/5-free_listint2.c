ed22d8c1518e59ac352112d904b18aa6dba682fa
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-04-10 19:22:27 +0000

    free list when head cointains nothing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: first node address
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *next_n;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		next_n = temp->next;
+		free(temp);
+		temp = next_n;
+	}
+
+	*head = NULL;
+}
