2bafc82b108f6fe3726fc89a4bd3a23df368ea6c
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-04-20 00:26:32 +0000

    C - File I/O (create a file)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * create_file - create a new file
+ * @text_content: content to be written in the file
+ * @filename: name of file
+ *
+ * Return: 1 if file is create
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, result;
