e2bb9af70297e8d72ec3865c961fd8f59326061e
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-09-21 16:54:44 +0000

    Delete at an index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *tempb;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (index > 0 && temp != NULL)
+	{
+		temp = temp->next;
+		index--;
+	}
+	if (temp == NULL)
+		return (0);
+	tempb = temp->prev;
+	tempb->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = tempb;
+	free(temp);
+
+	return (1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,5 +29,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *insert_empty(dlistint_t **h, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /*LIST_H*/
