83dff65487757527b727f87c2e011286341768ae
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-09-21 13:49:06 +0000

    Add node to a position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,30 +1,41 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - insert node at a position
+ * @h: head of list
+ * @idx: position of node
+ * @n: data of node
+ * Return: new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tempb, *new_node, *tempf;
+	unsigned int count = 0;
+
 	tempb = *h;
 
 	while (idx != 1)
 	{
 		tempb = tempb->next;
 		idx--;
+		count++;
 	}
-	tempf = tempb->next;
+	if (count > idx)
+		return (NULL);
 
 	/* create new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
+	tempf = tempb->next;
 	tempb->next = new_node;
-	tempf->prev = new_node;
 	new_node->prev = tempb;
 	new_node->next = tempf;
-
+	if (tempf != NULL)
+		tempf->prev = new_node;
 	return (new_node);
 }
