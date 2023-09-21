d8ede64519781a156bc14160a6985c7624e3a6f1
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-09-21 14:44:01 +0000

    add node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+ * insert_empty - insert node when list is empty
+ * @h: head of list
+ * @n: data of node
+ * Return: new node
+ */
+dlistint_t *insert_empty(dlistint_t **h, int n)
+{
+	dlistint_t *temp;
+
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->next = *h;
+	temp->n = n;
+	temp->prev = NULL;
+
+	if (*h != NULL)
+		(*h)->prev = temp;
+	*h = temp;
+
+	return (temp);
+}
+/**
  * insert_dnodeint_at_index - insert node at a position
  * @h: head of list
  * @idx: position of node
@@ -10,19 +34,18 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tempb, *new_node, *tempf;
-	unsigned int count = 0;
 
+	if (idx == 0)
+		return (insert_empty(h, n));
 	tempb = *h;
 
 	while (idx != 1)
 	{
+		if (tempb == NULL || tempb->next == NULL)
+			return (NULL);
 		tempb = tempb->next;
 		idx--;
-		count++;
 	}
-	if (count > idx)
-		return (NULL);
-
 	/* create new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
