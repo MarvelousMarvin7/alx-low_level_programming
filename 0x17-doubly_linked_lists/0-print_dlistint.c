1227c3387b53a44b83b6a64e7f64759891206a35
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-10-12 23:33:47 +0000

    print element update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t n = 0;
-  while (h != NULL)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      n++;
-    }
-  return n;
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
