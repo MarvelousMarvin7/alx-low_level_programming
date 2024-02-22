4968ef0b79a26342e53c583c7db7e8d4fbf87d97
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-02-22 02:53:08 +0000

    Hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print data in hash table as dictionary
+ * @ht : hash table
+ *
+ * Return : Nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int count = 0, i = 0;
+
+	if (ht != NULL)
+	{
+		printf("{");
+		for (count = 0; count < ht->size; count++)
+		{
+			node = ht->array[count];
+			while (node != NULL)
+			{
+				if (i != 0)
+				{
+					printf(", ");
+				}
+				i = 1;
+				printf("'%s' : '%s'", node->key, node->value);
+				node = node->next;
+			}
+		}
+		printf("}\n");
+	}
+}
